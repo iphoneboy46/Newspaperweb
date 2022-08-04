3366850fd12fcd86148ee1f1518722e5d84fab1a
iphoneboy46 <64023175+iphoneboy46@users.noreply.github.com>
2022-08-04 22:57:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var menuBars = document.querySelector('.bars');
 var menuList = document.querySelector('.menu-responsive');
+var exits = document.querySelector('.exits');
 
 menuBars.onclick = function(e){
-    menuList.classList.toggle('show')
+   menuBars.style.display = 'none';
+   exits.style.display = 'block'
+   menuList.style.display = 'block';
+}
+
+exits.onclick = function(e){
+    exits.style.display = 'none';
+    menuBars.style.display = 'block'
+    menuList.style.display = 'none';
 }
